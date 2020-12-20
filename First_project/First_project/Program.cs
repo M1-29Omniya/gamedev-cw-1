c7fa92bd541723dfdb87569eb6d8dad17477476d
Omniya <omniya.f@outlook.com>
2020-12-20 20:04:10 +0300

    Update Program.cs

---
+++ First_project/First_project/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace First_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 15;
+            double speed = 1.65;
+            String characterName = "Ahmed", superPower1 = "Strong Aromer", superPower2 = "Night Vision";
+            Console.WriteLine("\nMy name is " + characterName + ".\nMy Super powers are " + superPower1 + " and " + superPower2 + ". \nMy age is " + age + ".\nMy speed is "+speed);
+        }
+    }
+}
