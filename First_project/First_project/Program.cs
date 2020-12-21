afd8e78646dd31aefb1c03055eb720a2f6c4fb5b
Omniya <omniya.f@outlook.com>
2020-12-21 17:54:24 +0300

    Update Program.cs

--- First_project/First_project/Program.cs
+++ First_project/First_project/Program.cs
@@ -12,7 +12,27 @@ namespace First_project
             Console.WriteLine("\nMy name is " + characterName + 
                               ".\nMy Super powers are " + superPower1 + " and " + superPower2 + 
                               ". \nMy age is " + age + ".\nMy speed is "+speed+".");
+                              ". \nMy age is " + age + ".\nMy speed is "+speed+".\n");
 
+            //Another Hero
+
+            String heroSuperPower1 = "Strength", heroSuperPower2 = "stealth", heroName = "Kasandra";
+            int heroAge = 50;
+            double heroHeight = 190.5;
+            int ageDifference;
+
+            if (age > heroAge)
+            {
+                ageDifference = age - heroAge;
+            }
+            else
+            {
+                ageDifference = heroAge - age;
+            }
+            Console.WriteLine("\nMy name is " + heroName +
+                              ".\nMy Super powers are " + heroSuperPower1 + " and " + heroSuperPower2 +
+                              ". \nMy age is " + heroAge + ".\nMy height is " + heroHeight + ".\n");
+            Console.WriteLine("Age difference is "+ageDifference);
         }
     }
 }
